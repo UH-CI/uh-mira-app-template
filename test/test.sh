2c2897e21a52025be494e15358cba298d91d20de
Sean B. Cleveland <sean.b.cleveland@gmail.com>
2016-01-12 10:12:34 -0700

    added test script and confirmed execution
    added test script and confirmed execution

--- test/test.sh
+++ test/test.sh
@@ -1,1 +1,35 @@
-test.sh
+#!/bin/bash
+#set -x
+
+# These variables correspond to the inputs and parameter ids from the apps.json file. 
+# The API will pass the actual values into this script prior to execution.
+MANIFEST=~/manifest.mira 
+
+# Conditional create an input/output directory in the local scratch folder
+if [ ! -d input ]; then mkdir input; fi
+if [ ! -d output ]; then mkdir output; fi
+
+# Copy from source directory to the input directory
+cp $MANIFEST input/
+
+# Figure out the filename that has been copied into input/
+# Here, I use a clever trick for grabbing the last field of any slash-delim path - it's similar in 
+# function to basename, but is a simple string operation rather than a system call. It can therefore
+# work on URLs as well as iRODS paths and local filesystem paths.
+#
+# See http://tldp.org/LDP/LGNET/18/bash.html for more cool stuff like this.
+#
+FILENAME=${MANIFEST##*/} 
+source ~/.bash_profile
+module load prod/mira_4.0.2
+
+# Check for existence of input file...
+if [ -e input/$FILENAME ]; then
+	
+	
+	 mira input/$FILENAME > output/mira-app_output.txt
+
+fi
+
+# no need to stage out data, your output will be archived for you.
+
